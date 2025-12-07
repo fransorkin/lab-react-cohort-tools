3cfbdbc566ece1ecca57511d20bf1e6d31693acc
fransorkin <sorkinfrancisco@gmail.com>
2025-12-07 20:15:37 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
-
 function App() {
-
   return (
-    <div className="App relative z-20 pt-20">
-      <Navbar />
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-      <div className="pages">
-        <HomePage />
+        <Route path="/students/:studentId" element={<StudentDetailsPage />} />
 
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
     </div>
   );
 }
